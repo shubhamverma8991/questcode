a1e799f2c586a2b696cc5e5bea1940e4a73dcaad
Shubham Verma <shubhamv2010@gmail.com>
2024-12-31 12:42:36 +0530

    Header Fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,8 @@ const Header = () => {
   return (
     <header className="bg-card text-white flex items-center justify-center h-16 shadow-md mb-2">
       <div className="flex items-center hover:cursor-pointer" onClick={() => navigate("/")}>
-        <img src={logo} alt="QuestCode Logo" className="h-10 w-10 mr-2" />
-        <h1 className="text-2xl font-bold text-primary">PrepQuest</h1>
+        <img src={logo} alt="PrepQuest Logo" className="h-10 w-10 mr-2" />
+        <h1 className="text-3xl font-bold text-primary">PrepQuest</h1>
       </div>
     </header>
   );
